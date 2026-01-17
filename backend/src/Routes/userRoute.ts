7454af25e5c47c5b9d6a7e99235f49813330d105
Mustafa melake <mustafamelake@gmail.com>
2026-01-18 00:37:46 +0200

    Register page

--- backend/src/Routes/userRoute.ts
+++ backend/src/Routes/userRoute.ts
@@ -14,7 +14,7 @@ router.post("/register", async (req, res) => {
       email,
       password,
     });
-    res.status(statusCode).send(data);
+    res.status(statusCode).json(data);
   } catch {
     res.status(500).send("Sth went wrong");
   }
@@ -26,7 +26,7 @@ router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     const { statusCode, data } = await login({ email, password });
-    res.status(statusCode).send(data);
+    res.status(statusCode).json(data);
   } catch {
     res.status(500).send("Sth went wrong");
   }
