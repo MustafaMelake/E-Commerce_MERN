863d1a9ab8995349f757d84d084b815cc4efe7c6
Mustafa melake <mustafamelake@gmail.com>
2026-01-21 17:09:22 +0200

    CkeckoutPage + OrderPage

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -4,6 +4,7 @@ import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
 import Container from "@mui/material/Container";
 import { useCart } from "../context/cart/cartContext";
+import { useNavigate } from "react-router-dom";
 
 const CartPage = () => {
   const {
@@ -13,6 +14,12 @@ const CartPage = () => {
     removeItemInCart,
     clearCart,
   } = useCart();
+
+  const navigate = useNavigate();
+
+  const handleCheckout = () => {
+    navigate("/checkout");
+  };
 
   const handleQuantity = (productID: string, quantity: number) => {
     if (quantity <= 0) {
@@ -67,10 +74,13 @@ const CartPage = () => {
           </ButtonGroup>
         </Box>
       ))}
-      <Box>
+      <Box display="flex" flexDirection="row" justifyContent="space-between">
         <Typography variant="h4">
           Total Amount: {totalNumber.toFixed(2)} EGP
         </Typography>
+        <Button variant="contained" onClick={handleCheckout}>
+          Go To Checkout
+        </Button>
       </Box>
     </Box>
   );
