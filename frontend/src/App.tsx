6dee5f73b0e8517ac16eeb64bf364745159ff1a8
Mustafa melake <mustafamelake@gmail.com>
2026-02-10 01:35:44 +0200

    Router update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
 import RegisterPage from "./pages/RegisterPage";
@@ -15,21 +15,18 @@ function App() {
   return (
     <AuthProvider>
       <CartProvider>
-        <BrowserRouter>
-          <Navbar />
-          <HashRouter>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route element={<ProtectedRoute />}>
-                <Route path="/cart" element={<CartPage />} />
-                <Route path="/checkout" element={<CheckoutPage />} />{" "}
-                <Route path="/order-success" element={<OrderSuccessPage />} />
-              </Route>
-            </Routes>
-          </HashRouter>
-        </BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />{" "}
+            <Route path="/order-success" element={<OrderSuccessPage />} />
+          </Route>
+        </Routes>
       </CartProvider>
     </AuthProvider>
   );
